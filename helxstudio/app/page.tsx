1e18b4ff1836d270a48bb36db21b3604226c6ccc
Hitesh Suthar <senorhitesh@gmail.com>
2026-07-22 20:55:13 +0530

    added pixel trail

--- helxstudio/app/page.tsx
+++ helxstudio/app/page.tsx
@@ -1,10 +1,18 @@
 import Navbar from "@/components/Advancments/Navbar";
 import FeatureProjects from "@/components/Features/FeaturedProjects";
 import Hero from "@/components/Hero/Hero";
-
+import PixelTrail from "@/components/fancy/background/pixel-trail";
 export default function Home() {
   return (
     <div className="flex   items-center justify-center bg-zinc-50 font-sans ">
+      <div className="absolute inset-0 z-1">
+        <PixelTrail
+          pixelSize={20}
+          delay={130}
+          fadeDuration={0}
+          pixelClassName="bg-[#f1ff76]"
+        />
+      </div>
       <main className=" w-full relative h-full max-w-[102rem] flex-col items-center border-x">
         <Navbar />
         <Hero />
