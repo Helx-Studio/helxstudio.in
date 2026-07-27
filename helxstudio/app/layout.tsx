7f79d67060f58ab8dd605a11b7ec6875f1a2ec81
Hitesh Suthar <senorhitesh@gmail.com>
2026-07-28 01:52:40 +0530

    added speed and views analytics

--- helxstudio/app/layout.tsx
+++ helxstudio/app/layout.tsx
@@ -3,8 +3,8 @@ import { Geist, Geist_Mono, Inter, Instrument_Serif } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "@/components/Advancments/SmoothScrool";
 import localFont from "next/font/local";
-// import { Analytics } from "@vercel/analytics/next"
-// import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const Mistral = localFont({
   variable: "--font-Main",
   src: "../components/font/ALTMistral-Medium.woff2",
@@ -45,8 +45,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <SmoothScroll>{children}</SmoothScroll>
         <script src="https://cdn.lordicon.com/lordicon.js"></script>
-        {/* <Analytics/> */}
-        {/* <SpeedInsights/> */}
+        <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
